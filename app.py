a50b0311221ed936bd7ffe5149d1c872a5c4f233
pedrog1212 <111242265+pedrog1212@users.noreply.github.com>
2024-02-13 18:05:34 +0000

    comm1

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # If the form is submitted, retrieve the name from the form data
+        name = request.form['name']
+        return render_template('submitted.html', name=name)
+    else:
+        # If it's a GET request, render the form
+        return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
