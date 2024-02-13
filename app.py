22c26a3e7c5f5fad574e1d51799676918e5bd242
pedrog1212 <111242265+pedrog1212@users.noreply.github.com>
2024-02-13 18:50:34 +0000

    comm4

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, render_template
 
-app = Flask(__name__)
+# app = Flask(__name__)
+# from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
